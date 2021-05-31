2e5663ff406baa50ed61a11841d1ab1447de6720
Ofir Shtrosberg <ofir.shtrosberg@live.biu.ac.il>
2021-05-31 14:46:17 +0300

    31.5

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
-class Node:
-    #value, left, right = None
+import hashlib
+
+
+def calculate_hash_for_constructor(value):
+    value_encode = value.encode('utf-8')
+    return hashlib.sha256(value_encode).hexdigest()
+
+
+class NodeOfMerkleTree(object):
     def __init__(self, value):
-        self.left = None
-        self.right = None
-        self.value = value
+        #self.value = value
+        self.hashed_value = calculate_hash_for_constructor(value)
+        self.left_node = None
+        self.right_node = None
 
-class MerkleTree:
+
+# merkel tree is an almost complete tree
+# we will use dynamic list for the tree
+
+
+class MerkelTree(object):
     def __init__(self):
-            self.root = None
+        self.nodesList = []
 
-    def insert_node(self, node):
-        # what if self null
-        if node.value < self.value: # left side
-            if self.left is not None:
-                self.insert_node(self.left, node)
+    def insert_node(self, value):
+        if value is None:
+            return
+        else:
+            self.nodesList.append(NodeOfMerkleTree(value))
+            # if its just root it doesnt have parent
+            if len(self.nodesList) == 1:
+                return
             else:
-                self.left = node
+                # update parent that this node is his child
+                last_index = len(self.nodesList) - 1
+                parent_index = (last_index - 1) / 2
+                # is left child
+                if len(self.nodesList) % 2 == 0:
+                    self.nodesList[parent_index].left_node = self.nodesList[last_index]
+                # is right child
+                else:
+                    self.nodesList[parent_index].right_node = self.nodesList[last_index]
 
-        if node.value > self.value: # right side
-            if self.right is not None:
-                self.insert_node(self.right, node.value)
-            else:
-                self.right = node
+    def update_tree_after_insert(self):
+        for i in range(0, len(self.nodesList)):
+            # i want to concatenate the values of both children if exist and then do hash on the result
 
-    def printTree(self, node):
-        if node is not None:
-            self.printTree(node.l)
-            print(str(node.v) + ' ')
-            self.printTree(node.r)
-
-def main():
-    node1 = Node(1)
-    node2 = Node(2)
-    node3 = Node(3)
-    node4 = Node(4)
-
-    mt = MerkleTree()
-    mt.insert_node(node1)
-    mt.printTree(node1)
-    # mt.insert_nod
-
+# main
