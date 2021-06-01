57b39a26749c5ebd7056ac08909203b2909cc085
laredo <laredo93i@gmail.com>
2021-06-01 13:47:57 +0300

    1,2,3,4,5,6

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import hashlib
 import math
 
+# from cryptography.hazmat.primitives.asymmetric import rsa
+# from cryptography.hazmat.backends import default_backend
+# from cryptography.hazmat.primitives import serialization
+# from cryptography.hazmat.primitives import hashes
+# from cryptography.hazmat.primitives.asymmetric import padding
+
+
+
 def calculate_hash_for_constructor(value):
     value_encode = value.encode('utf-8')
     return hashlib.sha256(value_encode).hexdigest()
@@ -8,7 +16,6 @@ def calculate_hash_for_constructor(value):
 
 class NodeOfMerkleTree:
     def __init__(self, value, index):
-        # self.value = value
         # self.hashed_value = calculate_hash_for_constructor(value)
         self.value = value
         self.left_node = None
@@ -137,6 +144,39 @@ class MerkelTree:
         return False
 
 
+    # def generate_keys(self): # input 5
+    #     private_key = rsa.genetate_private_key(public_exponent=65537,
+    #                                            key_size=2048,
+    #                                            backend=default_backend())
+    #     pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,
+    #                                     format=serialization.PrivateFormat.TraditionalOpenSSL,
+    #                                     encryption_algorithm=alg
+    #                                     )
+    #     alg = serialization.BestAvailableEncryption(password)
+    #     secret_key = pem
+    #
+    #     public_key = private_key.public_key()
+    #     pem = public_key.public_bytes(
+    #         encoding=serialization.Encoding.PEM,
+    #         format=serialization.PublicFormat.SubjectPublicKeyInfo
+    #     )
+    #
+    #     public_key = pem
+    #
+    #     return_value = str(private_key) + " " + str(public_key)
+    #     return return_value
+    #
+    # def generate_signature(self, private_key):
+    #     signature = private_key.sign(
+    #         self.nodesList[0].value
+    #         padding.PES(
+    #             mgf=padding.MGF1(hashes.SHA256()),
+    #             salt_length=padding.PSS.MAX_LENGTH
+    #         ),
+    #         hashes.SHA256()
+    #     )
+    #
+    #     # maybe need to add padding like Hemi did
 
 
     def print_heap(self): # just for check
